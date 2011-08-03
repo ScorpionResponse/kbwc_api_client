61b30b23970c30c0811c7468686c6a06b0aede25
Paul Moss <moss.paul@gmail.com>
2011-08-03 12:31:15 -0400

    Add a few more test cases

--- test/Rest.py
+++ test/Rest.py
@@ -20,6 +20,37 @@ class RestTest(unittest.TestCase):
         self.assertEqual(len(npg_json), 1)
         self.assertEqual(npg_xml[0]['id'], npg_json[0]['id'])
 
+    def testListProviders(self):
+        xml = self.client_xml.list_providers()
+        json = self.client_json.list_providers()
+        self.assertEqual(len(xml), len(json))
+
+    def testSearchProviders(self):
+        xml = self.client_xml.search_providers('Nature')
+        json = self.client_json.search_providers('Nature')
+        self.assertEqual(len(xml), len(json))
+
+    def testGetProvider(self):
+        npg_xml = self.client_xml.get_collection('NPG.journals')
+        npg_json = self.client_json.get_collection('NPG.journals')
+        self.assertEqual(len(npg_xml), 1)
+        self.assertEqual(len(npg_json), 1)
+        self.assertEqual(npg_xml[0]['id'], npg_json[0]['id'])
+
+    def testGetEntrybyID(self):
+        entry_xml = self.client_xml.get_entry('036f688a982b7a702aadd05003f9742e')
+        entry_json = self.client_json.get_entry('036f688a982b7a702aadd05003f9742e')
+        self.assertEqual(len(entry_xml), 1)
+        self.assertEqual(len(entry_json), 1)
+        self.assertEqual(entry_xml[0]['id'], entry_json[0]['id'])
+
+    def testGetEntrybyUID(self):
+        entry_xml = self.client_xml.get_entry('NPG.journals,1987357')
+        entry_json = self.client_json.get_entry('NPG.journals,1987357')
+        self.assertEqual(len(entry_xml), 1)
+        self.assertEqual(len(entry_json), 1)
+        self.assertEqual(entry_xml[0]['id'], entry_json[0]['id'])
+
 def suite():
     suite = unittest.makeSuite(RestTest, 'test')
     return suite
