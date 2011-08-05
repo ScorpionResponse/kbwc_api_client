1b7b1b279ae9eba7fec16d7081d4b9ff2a6cfc41
Paul Moss <moss.paul@gmail.com>
2011-08-05 10:38:57 -0400

    Moved tests around to separate local only tests from remote tests

--- test.py
+++ test.py
@@ -1,16 +1,26 @@
 import unittest
-import test.Rest
-import test.OpenURL
-import test.ApiClient
+import test.remote
+import test.local
 import sys
 
 
-def suite():
+def suite(inc_remote=False):
     suite = unittest.TestSuite()
-    suite.addTest(test.Rest.suite())
-    suite.addTest(test.OpenURL.suite())
-    suite.addTest(test.ApiClient.suite())
+    suite.addTest(test.local.ApiClient.suite())
+    if inc_remote:
+        suite.addTest(test.remote.Rest.suite())
+        suite.addTest(test.remote.OpenURL.suite())
     return suite
 
+
+include_remote_tests = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'remote':
+        print 'Including Remote tests.'
+        include_remote_tests = True
+
 runner = unittest.TextTestRunner(verbosity=2)
-runner.run(suite())
+runner.run(suite(include_remote_tests))
+
+
+    
---
+++ test/remote/__init__.py
@@ -0,0 +1,3 @@
+
+import Rest
+import OpenURL
