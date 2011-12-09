e8196deecf23bb1ecad8b7408bd1512c4f9902ed
Paul Moss <moss.paul@gmail.com>
2011-12-09 12:44:53 -0500

    Use the open access account for testing.

--- test/remote/Rest.py
+++ test/remote/Rest.py
@@ -9,10 +9,8 @@ class RestTest(unittest.TestCase):
     """
 
     def setUp(self):
-        #inst_id = 111637
-        #wskey = 'TjBKm4f7QdZwxUrvfnukshyAIPkCgt3ZieslDR23Z95rV8rmqU3gIFvKRRDaTwX4UwzoQtQYIbyCqEWe'
-        inst_id = 105357
-        wskey = None
+        inst_id = 111637
+        wskey = 'TjBKm4f7QdZwxUrvfnukshyAIPkCgt3ZieslDR23Z95rV8rmqU3gIFvKRRDaTwX4UwzoQtQYIbyCqEWe'
         self.client_json = Rest(inst_id, wskey, response_format="json")
         self.client_xml = Rest(inst_id, wskey)
 
